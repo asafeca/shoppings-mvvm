5d09808d93aca001f5d5e6a35e726ec17951bc96
Adelino Safeca <safeetyy@gmail.com>
2022-03-09 12:37:18 +0100

    main activity added ConstraintLayout

--- app/src/main/java/com/laas/shoppingsmvvm/MainActivity.kt
+++ app/src/main/java/com/laas/shoppingsmvvm/MainActivity.kt
@@ -3,13 +3,11 @@ package com.laas.shoppingsmvvm
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.laas.shoppingsmvvm.core.util.Resource
-import com.laas.shoppingsmvvm.data.adapter.ProductInfoAdapter
 import com.laas.shoppingsmvvm.data.adapter.listeners.ProductInfoListener
 import com.laas.shoppingsmvvm.databinding.ActivityMainBinding
 import com.laas.shoppingsmvvm.di.ShoppingsApp
@@ -40,6 +38,9 @@ class MainActivity : AppCompatActivity(), ProductInfoListener {
 
     private fun prepareFields() {
 
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+
         alertDialog = SpotsDialog.Builder().setContext(this).build()
         recyclerview = findViewById(R.id.recycler_view)
         alertDialog.setMessage(R.string.loading.toString())
@@ -50,6 +51,8 @@ class MainActivity : AppCompatActivity(), ProductInfoListener {
             startActivity(intent)
             finish()
         }
+
+        /*
 
         productViewModel.onGet { result ->
             when (result) {
@@ -77,6 +80,8 @@ class MainActivity : AppCompatActivity(), ProductInfoListener {
             }
 
         }
+
+        */
 
 
     }
